3466b1597ccf5b529094fb8371568bd388872b28
johnjohnlin <johnjohnlin@gmail.com>
2015-05-23 17:27:22 +0800

    Add a template yay

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <cassert>
+#include <cstdio>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+int main(int argc, char const* argv[])
+{
+	Solution s;
+	return 0;
+}

dde85d34d9a07fdeafe173945731288b44635cb7
johnjohnlin <johnjohnlin@gmail.com>
2015-09-07 19:19:18 +0800

    Upgrade template.cpp

--- template.cpp
+++ template.cpp
@@ -16,6 +16,30 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
 };
 
+pair<ListNode*, vector<ListNode>> CreateList(const vector<int> &v) {
+	const int n = v.size();
+	pair<ListNode*,vector<ListNode>> ret{nullptr, vector<ListNode>(n, {0})};
+	if (n == 0) {
+		return ret;
+	}
+	ret.first = ret.second.data();
+	auto &data = ret.second;
+	for (int i = 0; i < n; ++i) {
+		data[i].val = v[i];
+		data[i].next = data.data()+i+1;
+	}
+	data.back().next = nullptr;
+	return ret;
+}
+
+void PrintList(ListNode *head) {
+	while (head != nullptr) {
+		printf("%d->", head->val);
+		head = head->next;
+	}
+	puts("$");
+}
+
 inline void PrintVector(const vector<int> &v) {
 	for (int num: v) {
 		printf("%d ", num);
